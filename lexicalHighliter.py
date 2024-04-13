424cf84d75d2a4970c64fb71717786b8da484218
Piga962 <cesarjrmecinas@gmail.com>
2024-04-13 15:23:35 -0600

    update "literalsOperators"

--- lexicalHighliter.py
+++ lexicalHighliter.py
@@ -1,3 +1,5 @@
+import re
+
 # Abre el archivo en modo lectura
 with open('archivo.txt', 'r') as archivo:
     # Itera sobre cada línea del archivo
@@ -5,4 +7,23 @@ with open('archivo.txt', 'r') as archivo:
         # Imprime cada línea
         print(linea)
 
-print("xd")
+
+text = 'x =++ "a+b" - 3 * 4 /'
+literal_pattern = r'[(=)*(+)*(\-)*(\*)*(/)*(!)*(<)*(>)*][(\")*(\")*(\')*(\')*][(=)*(+)*(\-)*(/)*(!)*(<)*(>)*]'
+
+pattern2 =r'[=+\-\*\/!<>]'
+parts = re.split(r'["\']', text)
+outside_quotes = parts[::2]
+#([(\")*(\')*])
+
+matches = [re.findall(pattern2, part) for part in outside_quotes]
+
+print(parts)
+print(outside_quotes)
+print(matches)
+
+for match in matches:
+    for symbol in match:
+        print (symbol)
+
+#print(gmail_addresses)
