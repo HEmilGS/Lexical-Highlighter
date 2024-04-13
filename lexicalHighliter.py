ee2b9a48ebf174ab24416e7291eed1053a14f5ba
HEmilGS <hectoremil748@gmail.com>
2024-04-12 18:27:56 -0600

    Lectura de archivos txt

---
+++ lexicalHighliter.py
@@ -0,0 +1,6 @@
+# Abre el archivo en modo lectura
+with open('archivo.txt', 'r') as archivo:
+    # Itera sobre cada línea del archivo
+    for linea in archivo:
+        # Imprime cada línea
+        print(linea)
