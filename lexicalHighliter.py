6a16c29b776ef909a508fac3dd015934e57a1908
Piga962 <cesarjrmecinas@gmail.com>
2024-04-13 15:33:49 -0600

    update

--- lexicalHighliter.py
+++ lexicalHighliter.py
@@ -1,6 +1,28 @@
+import re
+
 # Abre el archivo en modo lectura
 with open('archivo.txt', 'r') as archivo:
     # Itera sobre cada línea del archivo
     for linea in archivo:
         # Imprime cada línea
         print(linea)
+
+text = 'x =++ "a+b" - 3 * 4 /'
+literal_pattern = r'[(=)*(+)*(\-)*(\*)*(/)*(!)*(<)*(>)*][(\")*(\")*(\')*(\')*][(=)*(+)*(\-)*(/)*(!)*(<)*(>)*]'
+
+pattern2 =r'[=+\-\*\/!<>]'
+parts = re.split(r'["\']', text)
+outside_quotes = parts[::2]
+#([(\")*(\')*])
+
+matches = [re.findall(pattern2, part) for part in outside_quotes]
+
+print(parts)
+print(outside_quotes)
+print(matches)
+
+for match in matches:
+    for symbol in match:
+        print (symbol)
+
+#print(gmail_addresses)
