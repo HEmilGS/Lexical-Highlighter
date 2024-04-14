af3a33a84dcd5a383bc287c6dff766790b056d59
Piga962 <cesarjrmecinas@gmail.com>
2024-04-13 19:18:28 -0600

    actualizacion

--- lexicalHighliter.py
+++ lexicalHighliter.py
@@ -7,25 +7,28 @@ with open('archivo.txt', 'r') as archivo:
     for linea in archivo:
         # Imprime cada línea
         lineas.append(linea)
-        print(linea)
 
 
-pattern2 =r'[=+\-\*\/!<>]' #Expresión regular para encontrar los símbolos de opeardores	
+operatorPattern =r'[=+\-\*\/!<>]' #Expresión regular para encontrar los símbolos de opeardores	
+literalsPattern = r'["True"]*["False"]*["None"]*' #Expresión regular para encontrar los literales
 
-parts = [] #Lista para almacenar las partes del texto que no contienen comillas
-outside_quotes = [] #Lista para almacenar las partes del texto que no contienen comillas
+text = "x = True xdasdf False"
+x = re.findall(literalsPattern,text)
+print(x)
+
+
+operatorParts = [] #Lista para almacenar las partes del texto que no contienen comillas
+operatorOutsideQuotes = [] #Lista para almacenar las partes del texto que no contienen comillas
 
 for line in lineas:
     #Itera sobre cada línea del texto
     line_parts = re.split(r'["\']', line) #Guarda las partes de la línea eliminando las comillas
     line_outside_quotes = line_parts[::2] #Elimina las partes que contenian comillas
-    parts += line_parts #Agrega las partes a la lista de partes
-    outside_quotes += line_outside_quotes #Agrega las partes a la lista de partes que no contienen comillas
-    matches = [re.findall(pattern2, part) for part in outside_quotes] #Encuentra los símbolos de operadores en las partes que no contienen comillas
+    operatorParts += line_parts #Agrega las partes a la lista de partes
+    operatorOutsideQuotes += line_outside_quotes #Agrega las partes a la lista de partes que no contienen comillas
+    matches = [re.findall(operatorPattern, part) for part in operatorOutsideQuotes] #Encuentra los símbolos de operadores en cada parte de las partes sin comillas
 
-print(parts)
-print(outside_quotes)
-print(matches)
+print(operatorParts)
 
 for match in matches:
     for symbol in match:
