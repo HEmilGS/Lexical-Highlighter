0b44dfd0f0a4a28af0b07d54bdd75236302ed6e7
Piga962 <cesarjrmecinas@gmail.com>
2024-04-21 18:51:26 -0600

    Ya quedo GGs

--- lexicalHighliter.py
+++ lexicalHighliter.py
@@ -9,22 +9,19 @@ with open('archivo.txt', 'r') as archivo:
         lineas.append(linea)
 
 def highlight_operators(line):
-    operatorPattern = r'[=+\-\*\/!<>]'
-    literalsPattern = r'(("[^"]*")|(\'[^\']*\'))|([0-9]*\.*[0-9]+)*'
-    line = re.sub(literalsPattern, r'<span class="literals">\g<0></span>', line)
-    parts = re.split(r'("[^"]*"|\'[^\']*\')', line)
-
-    # for i in range(0, len(parts), 2):  # Only apply to every other segment
-    #     print(parts[i])
-    #     print("salto de lineaxd")
-    #     parts[i] = re.sub(operatorPattern, r'<span class="operator">\g<0></span>', parts[i])
-    line = ''.join(parts)
-    #print(line)
+    operatorPattern = r'[=+\-\*\/!<>]' #Patron de los operadores
+    literalsPattern = r'(("[^"]*")|(\'[^\']*\'))|([0-9]*\.*[0-9]+)' #Patron de los literales
+    
+    line = re.sub(literalsPattern,r'__LITERAL1__\g<0>__LITERAL2__',line)# Reemplaza los literales con el texto __LITERAL__ al inicio y al final incluyendo el mismo literal
+    parts = re.split(r'("[^"]*"|\'[^\']*\')', line)# Separa la línea en partes, si encuentra unas comillas dobles o simples
+    for i in range(0, len(parts), 2):
+        parts[i] = re.sub(operatorPattern, r'__OPERATOR1__\g<0>__OPERATOR2__', parts[i])# Reemplaza los operadores con el texto __OPERATOR__ al inicio y al final incluyendo el mismo operador
+    line = ''.join(parts)# Une las partes de la línea con las modificaciones y las partes de las comillas
+    line = line.replace('__OPERATOR1__', '<span class="operator">').replace('__OPERATOR2__', '</span>')#Reemplaza el texto __OPERATOR__ por las etiquetas span de html
+    line = line.replace('__LITERAL1__', '<span class="literals">').replace('__LITERAL2__', '</span>')#Reemplaza el texto __LITERAL__ por las etiquetas span de html
     return line
 
-highlighted_lines = [highlight_operators(line) for line in lineas]
-
-print(lineas)
+highlighted_lines = [highlight_operators(line) for line in lineas] #Aplica la función highlight_operators a cada línea del texto
 
 with open('highlighted.html', 'w') as f:
     f.write('<html><head><style>.operator { color: red; } .literals {color: blue;}</style></head><body><pre><code>\n')
