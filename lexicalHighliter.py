5b36189b40afd251ad265f5b4e024bdf1338e0d9
HEmilGS <hectoremil748@gmail.com>
2024-04-24 19:15:47 -0600

    comentarios listos

--- lexicalHighliter.py
+++ lexicalHighliter.py
@@ -32,19 +32,23 @@ with open('archivo.txt', 'r') as archivo:
 def highlight_items(line):
     puntuactionPattern = r'["\[\](){},:.\]]' #Patron de los operadores
     commentsPattern = r'#.*$'
+    
+    line = re.sub(commentsPattern,r'__COMMENTS1__\g<0>__COMMENTS2__',line)# Reemplaza los literales con el texto __LITERAL__ al inicio y al final incluyendo el mismo literal
+
+
     parts = re.split(r'("[^"]*"|\'[^\']*\')', line)# Separa la línea en partes, si encuentra unas comillas dobles o simples para encontrar los operadores que no estén dentro de las comillas
     for i in range(0, len(parts), 2):# Itera sobre las partes de la línea
         parts[i] = re.sub(puntuactionPattern, r'__PUNTUACTION1__\g<0>__PUNTUACTION2__', parts[i])# Reemplaza los operadores con el texto __OPERATOR__ al inicio y al final incluyendo el mismo operador
     line = ''.join(parts)# Une las partes de la línea con las modificaciones y las partes de las comillas
     line = line.replace('__PUNTUACTION1__', '<span class="puntuaction">').replace('__PUNTUACTION2__', '</span>')#Reemplaza el texto __OPERATOR__ por las etiquetas span de html
-    line = line.replace('__LITERAL1__', '<span class="literals">').replace('__LITERAL2__', '</span>')#Reemplaza el texto __LITERAL__ por las etiquetas span de html
+    line = line.replace('__COMMENTS1__', '<span class="comments">').replace('__COMMENTS2__', '</span>')#Reemplaza el texto __LITERAL__ por las etiquetas span de html
     
     return line
 
 highlighted_lines = [highlight_items(line) for line in lineas] #Aplica la función highlight_items a cada línea del texto
 
 with open('highlighted.html', 'w') as f:
-    f.write('<html>\n  <head>\n      <style>.operator { color: red; } .literals {color: blue;} .puntuaction {color: purple;}</style>\n  </head>\n\n  <body>\n      <pre>\n        <code>\n')
+    f.write('<html>\n  <head>\n      <style>.operator { color: red; } .literals {color: blue;} .puntuaction {color: purple;} .comments {color: green;c}</style>\n  </head>\n\n  <body>\n      <pre>\n        <code>\n')
     f.write('\n'.join(highlighted_lines))
     f.write('\n        </code>\n      </pre>\n  </body>\n</html>')
 
